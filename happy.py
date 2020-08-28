d8454bf3a9be76ffc010b7478585baf54e7c4ef7
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-08-28 22:13:06 +0530

    done

--- happy.py
+++ happy.py
@@ -1,7 +1,7 @@
 import pyttsx3
 engine = pyttsx3.init()
-for i in range(100): 
-    text =input()
+for i in range(1000000): 
+    text =input('Write Text Here: ')
     if(text=='exit'):
         exit()
     else:
